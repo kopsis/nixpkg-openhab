e2157fa7057c3c96ca5cda45abfea3c51ed99c4d
David J. Kessler <dkessler@acm.org>
2025-07-07 13:10:43 -0700

    Modernized flake using agenix as an example.

--- flake.nix
+++ flake.nix
@@ -1,24 +1,32 @@
 {
+    description = "Simple packaging of OpenHAB home automation service";
+
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs";
         flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { flake-utils, nixpkgs, ... }:
-    let
+    outputs = { 
+        flake-utils, 
+        nixpkgs, 
+        ... 
+    }: let
         overlay = final: prev: rec {
             jdk-openhab = final.callPackage ./jdk.nix {};
             openhab = final.callPackage ./openhab.nix { };
             openhab-addons = final.callPackage ./openhab-addons.nix {};
         };
-        pkgs = system: import nixpkgs { inherit system; overlays = [ overlay ]; };
+
+        eachSystem = nixpkgs.lib.genAttrs ( [ "x86_64-linux" "aarch64-linux" ] );
     in {
-        inherit overlay;
-    } //  flake-utils.lib.eachDefaultSystem (system: {
-        packages = rec {
+        overlays.default = overlay;
+
+        formatter = eachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+        packages = eachSystem (system: {
             jdk-openhab = (pkgs system).jdk-openhab;
             openhab = (pkgs system).openhab;
             openhab-addons = (pkgs system).openhab-addons;
-        };
-    });
+        });
+    };
 }
