ad0e00dd0219b2f90d173add99c441a7d026b582
David J. Kessler <dkessler@acm.org>
2025-07-09 08:58:03 -0700

    Added nixpkgs input url to openhab flake.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.follows = "nix/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { 
